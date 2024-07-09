ebe7305b6d9985d0d510fd1d3f349d48b7ed0ef1
zhushanbo <zhushanbo@baidu.com>
2024-07-09 13:54:57 +0800

    no message

---
+++ test.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env ts-node
+// 深入解析ts-node https://www.python100.com/html/94529.html
+
+const hello: string = "Hello World!";
+console.log(hello)
+console.info('Hello TS')
+console.info('123')
+console.info('321')
+console.info('111')
